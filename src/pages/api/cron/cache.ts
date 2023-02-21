26f7a129295f509c5cde50b6b54c6ea74bd73ecc
Fergal Moran <fergal.moran@gmail.com>
2023-02-21 16:18:20 +0000

    Add cron

---
+++ src/pages/api/cron/cache.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ hello: "sailor" });
+};
+export default handler;
