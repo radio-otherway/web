a786f96816817d5878e946eb5547d2d7baeab731
Fergal Moran <fergal.moran@gmail.com>
2023-03-06 18:24:05 +0000

    Gitversion added

---
+++ scheduler/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyInformationalVersion("0.2.0-alpha.47+Branch.develop.Sha.5a8a1abe3bc2e618ed7be2e289b77bd26660bd48")]
