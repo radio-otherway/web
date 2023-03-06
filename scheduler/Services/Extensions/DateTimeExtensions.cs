cd6bc90cd2f66d79675c7ed54b2aaee80e3c7e6a
Fergal Moran <fergal.moran@gmail.com>
2023-03-06 19:42:07 +0000

    Move to non mono-repo

--- scheduler/Services/Extensions/DateTimeExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace OtherWay.Radio.Scheduler.Services.Extensions;
-
-public static class DateTimeExtensions {
-  public static string ToLongDateTimeString(this DateTime date) {
-    return $"{date.ToLongDateString()} {date.ToLongTimeString()}";
-  }
-}
