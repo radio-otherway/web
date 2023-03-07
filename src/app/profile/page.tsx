d4770b6508df7814db5bf78a17c5bfd096a373f7
Fergal Moran <fergal.moran@gmail.com>
2023-03-07 08:03:57 +0000

    Fix not logged in profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,44 @@
+"use client";
 import { ProfilePageComponent } from "@/components/pages/profile";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useSigninCheck } from "reactfire";
+import Loading from "../loading";
 
-const ProfilePage = ({ params, searchParams }: {
+const ProfilePage = ({
+  params,
+  searchParams,
+}: {
   params?: { page: number };
-  searchParams?: { [key: string]: string | string[] | undefined }
+  searchParams?: { [key: string]: string | string[] | undefined };
 }) => {
-  return <ProfilePageComponent
-    page={searchParams && searchParams["page"] ? Number(searchParams["page"]) : 0}
-    onboarding={searchParams && searchParams["onboarding"] === "1" || false} />;
+  const { status, data: signInCheckResult } = useSigninCheck();
+  const router = useRouter();
+  useEffect(() => {
+    if (status !== "loading" && !signInCheckResult.signedIn) {
+      return router.push("/");
+    }
+  }, [status, signInCheckResult, router]);
+  if (status === "loading") {
+    return <Loading />;
+  }
+
+  if (signInCheckResult.signedIn === true) {
+    return (
+      <ProfilePageComponent
+        page={
+          searchParams && searchParams["page"]
+            ? Number(searchParams["page"])
+            : 0
+        }
+        onboarding={
+          (searchParams && searchParams["onboarding"] === "1") || false
+        }
+      />
+    );
+  } else {
+    <div>Waiting...</div>;
+  }
 };
 
 export default ProfilePage;
